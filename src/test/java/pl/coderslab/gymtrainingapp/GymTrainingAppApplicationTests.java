499d2059eb62ed06d6ee640d8377c781a49ec2b1
19Jumbo95 <maciekchodkowski@gmail.com>
2023-05-07 11:57:57 +0200

    first commit

---
+++ src/test/java/pl/coderslab/gymtrainingapp/GymTrainingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.gymtrainingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrainingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
